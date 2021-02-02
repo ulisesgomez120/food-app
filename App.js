930e861f19a3ab1d52eacf89129fcf5aabdfe341
Ulises Gomez <ulisesgomez120@gmail.com>
2021-02-02 10:57:43 -0600

    added some navigation libraries, and initial screen

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  console.log(Math.random());
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity style={[styles.flowBtnSecondary, styles.flowBtn]}>
+        <Text>I'm Picky</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.flowBtn}>
+        <Text>I'm Ready</Text>
+      </TouchableOpacity>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -14,8 +20,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  flowBtn: {
+    backgroundColor: "gray",
+    padding: 44,
   },
 });
