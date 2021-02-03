73ce9df330fd9db9fedef94b20cfe556a7503ca1
Ulises Gomez <ulisesgomez120@gmail.com>
2021-02-02 18:38:21 -0600

    added logic for navigation, and some initial screens

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const { Screen, Navigator } = createStackNavigator();
 
 export default function App() {
   console.log(Math.random());
   return (
-    <View style={styles.container}>
-      <TouchableOpacity style={[styles.flowBtnSecondary, styles.flowBtn]}>
-        <Text>I'm Picky</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.flowBtn}>
-        <Text>I'm Ready</Text>
-      </TouchableOpacity>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <Navigator initialRouteName='home'>
+        <Screen name='home' component={Home} options={{ headerShown: false }} />
+        <Screen name='filters' component={Filters} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
-
+const Home = ({ navigation }) => {
+  return (
+    <View style={{ marginTop: 44 }}>
+      <Text>Home</Text>
+      <Button title='Filters' onPress={() => navigation.navigate("filters")} />
+    </View>
+  );
+};
+const Filters = ({ navigation }) => {
+  return (
+    <View>
+      <Button title='home' onPress={() => navigation.navigate("home")} />
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
